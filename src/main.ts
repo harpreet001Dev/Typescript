5e4b0e599be1e5fdb4c6da0d52e3d4798b7bca43
harpreet001Dev <140056577+harpreet001Dev@users.noreply.github.com>
2025-11-01 16:03:00 +0530

    added refined cors

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- app.enableCors({
-  origin: [
-    'http://localhost:5173',
-    'https://typescript-frontend-topaz.vercel.app'
-  ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-});
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://typescript-frontend-topaz.vercel.app'
+    ],
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   await app.listen(process.env.PORT ?? 3333, '0.0.0.0');
 }
 bootstrap();
